fc39bfc7eb58bc1af4230653c8bdccc475f624b4
Rdawani <Romaildawani@gmail.com>
2024-03-23 12:21:49 -0700

    casting

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -6,6 +6,21 @@
 #include <cstdint>
 
 int main(){
+    int fahren = 100;
+    int celsius;
+
+    celsius = ((float)5/9) * (fahren-32);
+
+    std::cout<< "Fahreniet = " << fahren << std::endl;
+    std::cout<< "Celsius = " << celsius << std::endl;
+
+    float weight = 10.99;
+
+    std::cout<< "Integral part : "<<int(weight) << std::endl;
+    std::cout<< "Fractional part :"<< (double)((weight - int(weight))*10000)<<std::endl;
+
+
+
     
     std::cout << std::endl << std::endl;
     return (0);
